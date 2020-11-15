3ac7fd927014dea6cd868279e2a6d7eec8c71437
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-15 18:41:23 -0500

    Updating App.js to display all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,25 @@
 import React, { Component } from "react";
-import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 function App() {
   return (
     <Router>
       <Nav />
       <Header />
+      <Switch>
+          <Route exact path="/">
+            <Search />
+          </Route>
+          <Route path="/Saved">
+            <Saved />
+          </Route>
+        </Switch>
     </Router>
   );
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <div className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <h2>Welcome to React</h2>
-//       </div>
-//       <p className="App-intro">
-//         To get started, edit <code>src/App.js</code> and save to reload.
-//       </p>
-//     </div>
-//   );
-// }
-
 
 export default App;
