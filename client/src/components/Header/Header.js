a90b3bf0663fdf4c4f30dbf9c4332efd7b8396a7
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-15 10:16:19 -0500

    Creating Nav & Header components

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+const Header = (props) => (
+    <div className="text-center">
+            <h1>(React) Google Books Search</h1>
+            <p>Search for and Save Books of Interest</p>
+    </div>
+);
+
+export default Header;
