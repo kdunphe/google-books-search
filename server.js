44aff706244fc6c1d10ba2adb01224fbc3ad178c
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-14 20:20:57 -0500

    Connecting the database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,18 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
+
+// Connect to MongoDB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // Send every other request to the React app
 // Define any API routes before this runs
